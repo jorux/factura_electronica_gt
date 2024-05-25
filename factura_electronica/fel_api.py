ab23d41325e92fe38c39dfe8f39bbbdbda3d7f08
Rodrigo C <mistermaster0@gmail.com>
2024-05-25 12:52:59 -0600

    test

--- factura_electronica/fel_api.py
+++ factura_electronica/fel_api.py
@@ -1070,13 +1070,10 @@ def is_valid_to_fel(doctype, docname):
     #Condiciones para FEL Sales Invoice -> Nota abono -- Eliminar esto y dejar nota de credito con un extra para el return.against.
     elif (docinv.doctype == 'Sales Invoice') and (docinv.docstatus == 1) and (docinv.is_return == 1):
 
-        # Validacion de serie - Depende de Credit Note
-        active = frappe.db.exists('Configuracion Factura Electronica', {'name': config_name, 'nota_credito_fel': 1})
-
-        if val_serie and active:
-            values = frappe.db.get_values('Configuracion Series FEL',
-                                          filters={'parent': config_name, 'serie': docinv.naming_series},
-                                          fieldname=['tipo_documento'], as_dict=1)
+        #if val_serie and active:
+            #values = frappe.db.get_values('Configuracion Series FEL',
+                                          #filters={'parent': config_name, 'serie': docinv.naming_series},
+                                          #fieldname=['tipo_documento'], as_dict=1)
             return values[0]['tipo_documento'], 'valido', True
         else:
             return _('Serie de documento para nota de abono electrónica no configurada, \
