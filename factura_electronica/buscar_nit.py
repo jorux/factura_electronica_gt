cac0a088c09077cc3c6debd4cb2c6cdae199dba8
Rodrigo C. <mistermaster0@gmail.com>
2025-03-11 20:36:40 -0600

    corrigiendo mensaje de push

--- factura_electronica/buscar_nit.py
+++ factura_electronica/buscar_nit.py
@@ -6,11 +6,9 @@ def buscar_nombre (doc, method):
     """
     try:
         # Your custom logic here
-        frappe.log_error(f"Custom script executed for Sales Invoice: {doc.name}", "Sales Invoice On Submit")
         # Example: Accessing Sales Invoice data
         llave = frappe.db.get_value('Configuracion Factura Electronica', {'name': self.__config_name}, 'llave_ws')
         nit_company = str(frappe.db.get_value('Company', {'name': self.dat_fac[0]['company']}, 'nit_face_company').replace('-', '')).upper().strip()
-
         frappe.log_error(f"Customer: {customer}, llave: {llave}, nit {nit_company}", "Sales Invoice On Submit Info")
 
         #Add here the code that interacts with the guatemalan electronical invoice system.
