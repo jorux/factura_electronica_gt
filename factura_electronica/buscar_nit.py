3dfbe0828a7c27f9aee7617ceb8d1c8c3dd6b40e
Rodrigo C. <mistermaster0@gmail.com>
2025-03-11 20:52:07 -0600

    corregida funcion self

--- factura_electronica/buscar_nit.py
+++ factura_electronica/buscar_nit.py
@@ -7,9 +7,8 @@ def buscar_nombre (doc, method):
     try:
         # Your custom logic here
         # Example: Accessing Sales Invoice data
-        llave = frappe.db.get_value('Configuracion Factura Electronica', {'name': self.__config_name}, 'llave_ws')
-        nit_company = str(frappe.db.get_value('Company', {'name': self.dat_fac[0]['company']}, 'nit_face_company').replace('-', '')).upper().strip()
-        frappe.log_error(f"Customer: {customer}, llave: {llave}, nit {nit_company}", "Sales Invoice On Submit Info")
+        llave = frappe.db.sql("SELECT `tabConfiguracion Factura Electronica`.`llave_ws` AS `llave_ws`FROM`tabConfiguracion Factura Electronica`WHERE`tabConfiguracion Factura Electronica`.`docstatus` = 1")
+        frappe.log_error(f", llave: {llave}, nit {doc.nit_face_customer}", "Sales Invoice On Submit Info")
 
         #Add here the code that interacts with the guatemalan electronical invoice system.
         #For example, sending the invoice data to the API.
