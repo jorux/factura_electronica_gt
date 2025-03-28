d6a3328139d563f64f77201dbcea535d4ae48812
Rodrigo C. <mistermaster0@gmail.com>
2025-03-28 13:45:10 -0600

    Pasaporte OK. ahora se puede facturar con pasaporte

--- factura_electronica/fel/fel.py
+++ factura_electronica/fel/fel.py
@@ -718,7 +718,7 @@ class ElectronicInvoice:
             # Usar solo para debug
             with open('PREVIEW-FACTURA-FEL.xml', 'w') as f:
                 f.write(self.__xml_string)
-            frappe.log_error(f"XML Generado", self.__xml_string)
+            #frappe.log_error(f"XML Generado", self.__xml_string) - activar para ver el xml generado y debuggear
         except:
             return False, 'La peticion no se pudo convertir a XML. Si la falla persiste comunicarse con soporte'
 
