33a7802d2a8087460996fe97f27718e7408011b1
Rodrigo C. <mistermaster0@gmail.com>
2025-05-21 08:34:06 -0600

    Pasaporte OK. ahora se puede facturar con pasaporte. fix para customer name en pasaporte

--- factura_electronica/fel/fel.py
+++ factura_electronica/fel/fel.py
@@ -357,7 +357,7 @@ class ElectronicInvoice:
                     self.__d_receptor = {
                         "@CorreoReceptor": dat_direccion[0].get('email_id', datos_default.get('email')),
                         "@IDReceptor": str(self.dat_fac[0]['nit_face_customer']),  # NIT
-                        "@NombreReceptor": str(self.dat_fac[0]["customer_name"]),
+                        "@NombreReceptor": str(self.dat_fac[0]["customer"]),
                         "@TipoEspecial": str('EXT'), 
                         "dte:DireccionReceptor": {
                             "dte:Direccion": dat_direccion[0].get('address_line1', datos_default.get('address')),
